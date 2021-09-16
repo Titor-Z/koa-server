428833368d348243c7d8a46cd5b746f84645e12b
Titor <foolsecret@163.com>
2021-09-17 00:46:51 +0800

    feat✨:  nodemon

--- app/index.ts
+++ app/index.ts
@@ -2,8 +2,11 @@ import Application from "koa"
 
 const server = new Application()
 
-server.use(async ctx=>{
-    console.log(ctx)
+server.use(async (ctx) => {
+  ctx.body = "123213123213"
+  console.log(ctx.body)
 })
 
-server.listen(3000)
+server.listen(3000, () => {
+  console.log("server is running...")
+})
